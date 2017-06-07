5a6a1a77c1d15fd33714255e263504c758a182f5
FanJiana <Jiana@192-168-1-3.tpgi.com.au>
2017-06-07 17:59:14 +1000

    modify App.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import { store } from './store/config-store'
-import Root from './routers/index';
-import Layout from './containers/app-layout'
+import App from './App'
 import registerServiceWorker from './registerServiceWorker';
 
-render(
-    <Layout>
-        <Provider store={store}>
-            <Root />
-        </Provider>
-    </Layout>,
-    document.getElementById('root'));
+render(<App />, document.getElementById('root'));
 registerServiceWorker();
