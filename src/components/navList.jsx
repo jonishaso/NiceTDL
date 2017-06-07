8969bb543a207ef6ea472f4f76f94ce457acf5ec
FanJiana <Jiana@192-168-1-3.tpgi.com.au>
2017-06-07 21:50:58 +1000

    try use absolute url

--- src/components/navList.jsx
+++ src/components/navList.jsx
@@ -5,13 +5,13 @@ var NavList = () => {
     <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
       <ul className="navbar-nav">
       <li className="nav-item">
-        <a className="nav-link" href="/home">Home <span className="sr-only">(current)</span></a>
+        <a className="nav-link" href="https://jonishaso.github.io/nice-tdl/home">Home <span className="sr-only">(current)</span></a>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="/list">Todo List</a>
+        <a className="nav-link" href="https://jonishaso.github.io/nice-tdl/list">Todo List</a>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="/game">Game</a>
+        <a className="nav-link" href="https://jonishaso.github.io/nice-tdl/game">Game</a>
       </li>
     </ul>
     </nav>)
